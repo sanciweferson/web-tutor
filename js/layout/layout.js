905b18a476dab617c874573375b2e29eb72ccbf0
Sanciweferson <san@san.localdomain>
2026-02-04 15:25:43 -0300

    atuali

--- js/layout/layout.js
+++ js/layout/layout.js
@@ -3,43 +3,46 @@
 
 const ICONS = {
   home: `
-    <svg class="icon" viewBox="0 0 24 24" aria-hidden="true">
+    <svg class="icon home" viewBox="0 0 24 24" aria-hidden="true">
       <path d="M12 3 2 12h3v9h6v-6h2v6h6v-9h3z"/>
     </svg>
   `,
   list: `
-    <svg class="icon" viewBox="0 0 24 24" aria-hidden="true">
+    <svg class="icon " viewBox="0 0 24 24" aria-hidden="true">
       <path d="M3 5h18v2H3zm0 6h18v2H3zm0 6h18v2H3z"/>
     </svg>
   `,
   text: `
-    <svg class="icon" viewBox="0 0 24 24" aria-hidden="true">
+    <svg class="icon " viewBox="0 0 24 24" aria-hidden="true">
       <path d="M4 5h16v2H4zm0 4h16v2H4zm0 4h10v2H4z"/>
     </svg>
   `,
   bulb: `
-    <svg class="icon" viewBox="0 0 24 24" aria-hidden="true">
+    <svg class="icon " viewBox="0 0 24 24" aria-hidden="true">
       <path d="M9 21h6v-1H9zm3-20C7.935 1 5 3.935 5 7c0 2.386 1.19 4.486 3 5.74V17h8v-4.26c1.81-1.254 3-3.354 3-5.74 0-3.065-2.935-6-7-6z"/>
     </svg>
   `,
   user: `
-    <svg class="icon" viewBox="0 0 24 24" aria-hidden="true">
+    <svg class="icon usu" viewBox="0 0 24 24" aria-hidden="true">
       <path d="M12 12a5 5 0 1 0-5-5 5 5 0 0 0 5 5zm0 2c-4.418 0-8 2.239-8 5v3h16v-3c0-2.761-3.582-5-8-5z"/>
     </svg>
   `,
   mail: `
-    <svg class="icon" viewBox="0 0 24 24" aria-hidden="true">
+    <svg class="icon con" viewBox="0 0 24 24" aria-hidden="true">
       <path d="M2 4h20v16H2zm10 7L4 6v12h16V6z"/>
     </svg>
   `,
   facebook: `
-    <svg class="icon" viewBox="0 0 24 24" aria-hidden="true">
+    <svg class=" face " viewBox="0 0 24 24" aria-hidden="true">
       <path d="M22 12a10 10 0 1 0-11.5 9.9v-7h-2v-3h2V9.5c0-2 1.2-3.1 3-3.1.9 0 1.8.1 1.8.1v2h-1c-1 0-1.3.6-1.3 1.2V12h2.3l-.4 3h-1.9v7A10 10 0 0 0 22 12z"/>
     </svg>
   `,
   instagram: `
-    <svg class="icon" viewBox="0 0 24 24" aria-hidden="true">
-      <path d="M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5zm5 5a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm6-1a1 1 0 1 0-1 1 1 1 0 0 0 1-1z"/>
+  <svg class=" insta" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-instagram-icon lucide-instagram"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
+  `,
+    github: `
+    <svg class=" git" viewBox="0 0 24 24" aria-hidden="true">
+      <path d="M12 .5C5.73.5.5 5.74.5 12.02c0 5.11 3.29 9.44 7.86 10.97.57.11.78-.25.78-.56 0-.28-.01-1.02-.02-2-3.2.7-3.88-1.54-3.88-1.54-.53-1.35-1.3-1.71-1.3-1.71-1.06-.73.08-.72.08-.72 1.17.08 1.79 1.2 1.79 1.2 1.04 1.78 2.73 1.27 3.4.97.1-.75.41-1.27.74-1.56-2.55-.29-5.23-1.28-5.23-5.7 0-1.26.45-2.29 1.19-3.1-.12-.29-.52-1.47.11-3.07 0 0 .97-.31 3.18 1.18a11.02 11.02 0 0 1 5.8 0c2.2-1.49 3.17-1.18 3.17-1.18.64 1.6.24 2.78.12 3.07.74.81 1.19 1.84 1.19 3.1 0 4.43-2.69 5.4-5.25 5.69.42.36.79 1.08.79 2.18 0 1.57-.02 2.84-.02 3.23 0 .31.21.68.79.56A11.52 11.52 0 0 0 23.5 12C23.5 5.74 18.27.5 12 .5z"/>
     </svg>
   `
 };
@@ -86,9 +89,10 @@ const navLinks = [
 
 const socialItems = [
   { href: "https://www.facebook.com/", icon: "facebook", text: "" },
-  { href: "https://www.instagram.com/", icon: "instagram", text: "" }
+  { href: "https://www.instagram.com/", icon: "instagram", text: "" },
+  { href: "https://www.github.com/", icon: "github", text: "" }
 ];
- 
+
 
 // ---------- componente NAV ----------
 class NavBar extends HTMLElement {
